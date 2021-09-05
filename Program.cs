d2f4b4c4738e1ce21cfa4a8062d43ecf922e989a
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-05 15:20:36 -0300

    Adiciona atividade Gritador

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 2 - Faça um programa repita exatamente que o usuário digitar, gritando (em CAIXA ALTA).
+
+            Console.Write("Você fala: ");
+            string answer = Console.ReadLine();
+            Console.WriteLine($"\nO Gritador repete: {answer.ToUpper()}");
+        }
+    }
+}
